6ed0bc1464df476422cfc7b8625ec7fbcf9f597c
TheeChody <chodywhooch@live.ca>
2024-04-24 17:41:12 -0600

    Reworked quite a bit Mostly added a dedicated downloaded folder

--- Alpha/main.py
+++ Alpha/main.py
@@ -1,17 +1,29 @@
 import os
+import sys
 import subprocess
 import customtkinter
 from pathlib import Path
 from pytube import YouTube
 
-folder_path = Path(__file__).parent.absolute()
 video_list = ["Audio Only", "1080p", "720p", "Lowest Res"]
-Path(f"{folder_path}/pytube/__cache__").mkdir(parents=True, exist_ok=True)
 illegal_chars = ("#", "%", "&", "{", "}", "\\", "<", ">", "*", "?", "/", "$", "!", "'", '"', ":", "@", "+", "`", "|", "=")
+
+if getattr(sys, 'frozen', False):
+    application_path = os.path.dirname(sys.executable)
+else:
+    application_path = os.path.dirname(__file__)
+
+downloads_path = f"{application_path}/Thee Downloaded/"
+Path(f"{application_path}/Thee Downloaded").mkdir(parents=True, exist_ok=True)
+Path(f"{Path(__file__).parent.absolute()}/pytube/__cache__").mkdir(parents=True, exist_ok=True)
 
 
 def start_download():
     try:
+        if dl_link.get() is None:
+            download_completed.configure(text=f"URL Field Is Blank")
+            reset_app(True)
+            return
         download_completed.configure(text="")
         title.configure(text="Processing Request")
         hd_res, audio_only, audio_download, new_filename = False, False, None, ""
@@ -35,7 +47,7 @@ def start_download():
             download_completed.configure(text=f"Error downloading file, must choose {', '.join(video_list)}")
             reset_app(True)
             return
-        filename = f"{youtube_object.author}--{youtube_object.title}{'.mp3' if audio_only else '.mp4'}"
+        filename = f"{youtube_object.author}--{youtube_object.title}.mp4"
         for letter in filename:
             if letter in illegal_chars:
                 letter = "X"
@@ -44,8 +56,16 @@ def start_download():
             new_filename += letter
         filename = new_filename
         download.download(filename=filename)
-        if hd_res:
-            audio_filename = filename[:-4] + ".mp3"
+        if audio_only:
+            audio_filename = f"{filename[:-4]}.mp3"
+            subprocess.run(f"ffmpeg -i {filename} {audio_filename}")
+            if os.path.exists(filename):
+                os.remove(filename)
+            if os.path.exists(audio_filename):
+                os.rename(audio_filename, f"{downloads_path}{audio_filename.replace('_', ' ')}")
+            download_completed.configure(text=f"{filename.replace('_', ' ')}\nAudio Downloaded")
+        elif hd_res:
+            audio_filename = f"{filename[:-4]}.mp3"
             audio_download.download(filename=audio_filename)
             comb_filename = f"combined_{filename}"
             subprocess.run(f"ffmpeg -i {filename} -i {audio_filename} -c copy {comb_filename}")
@@ -54,18 +74,22 @@ def start_download():
             if os.path.exists(audio_filename):
                 os.remove(audio_filename)
             if os.path.exists(comb_filename) and not os.path.exists(filename):
-                os.rename(comb_filename, filename)
-            download_completed.configure(text=f"{filename}\nDownloaded & Ready")
+                filename = filename.replace("_", " ")
+                os.rename(comb_filename, f"{downloads_path}{filename}")
+            download_completed.configure(text=f"{filename}\nVideo Downloaded & Ready")
         else:
-            download_completed.configure(text=f"{filename}\n{'Audio' if audio_only else 'Video'} Downloaded")
+            if os.path.exists(filename):
+                os.rename(filename, f"{downloads_path}{filename.replace('_', ' ')}")
+            download_completed.configure(text=f"{filename.replace('_', ' ')}\nVideo Downloaded")
         reset_app()
     except Exception as e:
-        if WindowsError:
-            download_completed.configure(text=f"Error Combining Video with Audio\nAre you sure you have FFmpeg installed and path set correctly?\nIf so, contact TheeChody\n\n{e}")
-            reset_app(True)
+        if FileExistsError:
+            download_completed.configure(text=f"Error there is a file that already exists with this filename!!\n\n{e}")
+        elif WindowsError:
+            download_completed.configure(text=f"Error Combining/Converting Video with/to Audio\nAre you sure you have FFmpeg installed and path set correctly?\n\nIf so, contact TheeChody with this error\n\n{e}")
         else:
-            download_completed.configure(text=f"Error downloading file\n\n{e}\n\nContact TheeChody for more details")
-            reset_app(True)
+            download_completed.configure(text=f"Error downloading file\n\n{e}\n\nContact TheeChody with this error for more details")
+        reset_app(True)
         return
 
 
@@ -81,24 +105,24 @@ customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("green")
 
 app = customtkinter.CTk()
-app.geometry("700x450")
-app.title("Thee YT Downloader")
+app.geometry("600x420")
+app.title("Thee YouTube Downloader")
 
 title = customtkinter.CTkLabel(app, text="Enter YouTube LInk")
 title.pack(padx=10, pady=10)
 
 dl_link = customtkinter.StringVar()
-link_box = customtkinter.CTkEntry(app, width=600, height=30, textvariable=dl_link)
+link_box = customtkinter.CTkEntry(app, width=550, height=35, textvariable=dl_link)
 link_box.pack()
 
 video_res = customtkinter.StringVar()
-vid_res_list = customtkinter.CTkOptionMenu(app, values=video_list, variable=video_res, )
+vid_res_list = customtkinter.CTkOptionMenu(app, values=video_list, variable=video_res)
 vid_res_list.pack(padx=10, pady=10)
 
 download_button = customtkinter.CTkButton(app, text="Download", command=start_download)
 download_button.pack(padx=10, pady=10)
 
 download_completed = customtkinter.CTkLabel(app, text="")
-download_completed.pack(padx=10, pady=10)
+download_completed.pack(padx=20, pady=10)
 
 app.mainloop()
